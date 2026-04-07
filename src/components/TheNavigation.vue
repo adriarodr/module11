b9cd79363464c737fc55f3d1d3488c06ced6589e
adriarodr <158720918+adriarodr@users.noreply.github.com>
2026-04-06 21:06:08 -0400

    Remove commented out code and Change style.css

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,6 +1,5 @@
 <template>
   <nav id="nav">
-    <!-- <router-link to="/">Home</router-link> -->
     <router-link :to="{ name: 'about' }">About</router-link>
     <router-link :to="{ name: 'random.quotes' }">Random Quotes</router-link>
     <router-link :to="{ name: 'contact' }">Contact</router-link>
